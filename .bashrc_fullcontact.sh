7a34d45f8e0419f573ae7d316b1e17fd92c2003c
Eric Entzel <eric@ubermac.net>
2014-06-12 13:07:11 -0600

    Java8

--- .bashrc_fullcontact.sh
+++ .bashrc_fullcontact.sh
@@ -1,5 +1,6 @@
 export JAVA6=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 export JAVA7=/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home
+export JAVA8=/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home
 
 export GRADLE_HOME=~/3rdparty/gradle
 export PATH=${PATH}:${GRADLE_HOME}/bin
