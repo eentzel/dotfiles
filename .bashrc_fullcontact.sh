0a10aa6ce03f8c5fac805cf7ec3f7228ec5eb670
Eric Entzel <eric@ubermac.net>
2016-08-23 10:04:03 -0600

    .bashrc: Better way of getting java_home on OSX

--- .bashrc_fullcontact.sh
+++ .bashrc_fullcontact.sh
@@ -1,6 +1,7 @@
-export JAVA6=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
-export JAVA7=/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home
-export JAVA8=/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home
+export JAVA6=$(/usr/libexec/java_home -v 1.6)
+export JAVA7=$(/usr/libexec/java_home -v 1.7)
+export JAVA8=$(/usr/libexec/java_home -v 1.8)
+export JAVA_HOME=$JAVA8
 
 export GRADLE_HOME=~/3rdparty/gradle
 export PATH=${PATH}:${GRADLE_HOME}/bin
